45219d4728e536d9545fef25cea58785b52defb6
HVKukkonen <valtterik@msn.com>
2020-11-12 14:32:01 +0200

    moi hyvältä näyttää

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose')
 
 // specify schema
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
 // export mongoose object based on specified schema
 module.exports = mongoose.model('Blog', blogSchema)
